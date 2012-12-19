6a1d3379d7c1e5c8402ab6119d538ad7611b37d6
dirk lüsebrink <dirk.luesebrink@gmail.com>
2012-12-19 19:32:49 +0100

    work in progress
    pre 2.x.x plugins seem to have no need for routes?

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 #match 'doc_pu', :controller => 'doc_pu' # , :action => 'order_blocks', :via => :post
-match '/doc_pu' => 'doc_pu#index' #, :action => 'order_blocks', :via => :post
+#match '/doc_pu' => 'doc_pu#index' #, :action => 'order_blocks', :via => :post
+get 'doc_pu' => 'doc_pu#index' #, :action => 'order_blocks', :via => :post
 #get 'doc_pu', :to => 'doc_pu#index'
+match '/doc_pu/new', :controller=> 'doc_pu', :action => 'new', :via => :get
