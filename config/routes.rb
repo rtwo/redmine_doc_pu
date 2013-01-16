38a2a45a35f1b6778bd2bd82bbfb6614344da31d
dirk lüsebrink <dirk.luesebrink@gmail.com>
2013-01-16 18:53:07 +0100

    adding post requests

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@
 get 'doc_pu' => 'doc_pu#index' #, :action => 'order_blocks', :via => :post
 #get 'doc_pu', :to => 'doc_pu#index'
 #match '/doc_pu/new', :controller=> 'doc_pu', :action => 'new', :via => :get
-match '/doc_pu(/:action)', :controller => 'doc_pu' #, :via => :get
+match '/doc_pu(/:action)', :controller => 'doc_pu', :via => [:get, :post]
 match '/doc_pu_wiki(/:action)', :controller => 'doc_pu_wiki' #, :via => :get
